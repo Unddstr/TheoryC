3ab2e364a103fe8b3b5c3d2f0fcb8c6bdd9f033f
Alexander Panteleev <unddstr@gmail.com>
2023-03-11 15:11:44 +0300

    Является ли одно число квадратом 2-го?

---
+++ Seminar/Seminar2/Example004/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 4
+Console.Clear();
+Console.Write("Введите 1-е число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-е число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a * a == b || b * b == a)
+    Console.Write("yes");
+else
+    Console.Write("no");
