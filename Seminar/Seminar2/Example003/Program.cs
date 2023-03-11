a7e5cad4c70815366f725a615bc4a979ca7bcda8
Alexander Panteleev <unddstr@gmail.com>
2023-03-11 14:45:29 +0300

    Задача 3

---
+++ Seminar/Seminar2/Example003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 2
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n % m == 0)
+    Console.WriteLine("кратно");
+else
+    Console.WriteLine($"не кратно, остаток {n % m}");
