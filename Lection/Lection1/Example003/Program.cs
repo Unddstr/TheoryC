10127ce04e31cce09acb4f7d1a30b6e83252b85b
Alexander Panteleev <unddstr@gmail.com>
2023-03-11 12:38:15 +0300

    Сумма 2-х случайных чисел

---
+++ Lection/Lection1/Example003/Program.cs
@@ -0,0 +1,7 @@
+﻿// Сумма 2-х случайных чисел
+int a = new Random().Next(1, 10);
+Console.WriteLine(a);
+int b = new Random().Next(1, 10);
+Console.WriteLine(b);
+int result = a + b;
+Console.WriteLine(result);
