087ef48f97dd3b40ddd3e4ca06b4b5631189c053
Alexander Panteleev <unddstr@gmail.com>
2023-03-11 15:26:27 +0300

    Является ли число одновременно кратным двум другим числам?

--- Seminar/Seminar2/Example003/Program.cs
+++ Seminar/Seminar2/Example003/Program.cs
@@ -1,10 +1,26 @@
-﻿// Задача 2
-Console.Clear();
-Console.Write("Введите 1-ое число: ");
-int n = Convert.ToInt32(Console.ReadLine());
+﻿// Задача 3 Кратно ли число на вводе одновременно двум другим числам?
+// Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-ое число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-if (n % m == 0)
-    Console.WriteLine("кратно");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a % b == 0 && a % c == 0)
+    Console.WriteLine("yes");
 else
-    Console.WriteLine($"не кратно, остаток {n % m}");
+    Console.WriteLine("no");
+
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 3-е число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// if (a % (b * c) == 0)
+//     Console.WriteLine("кратно");
+// else
+//     Console.WriteLine("не кратно");
