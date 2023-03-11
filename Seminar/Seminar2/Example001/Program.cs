c1aa9a30d6a303b900c410e425b2149b34d1d467
Alexander Panteleev <unddstr@gmail.com>
2023-03-11 15:39:37 +0300

    Сгенерировать случайное двухзначное число и показать какое из его цифр больше

---
+++ Seminar/Seminar2/Example001/Program.cs
@@ -0,0 +1,10 @@
+﻿// 
+Console.Clear();
+int a = new Random().Next(10, 100);
+Console.WriteLine($"Случаной сгенированное число {a}");
+int b = a / 10;
+int c = a % 10;
+if (b > c)
+Console.WriteLine(b);
+else
+Console.WriteLine(c);
