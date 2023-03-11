4294d7dd8da01eaa1a608601ffbfc016ab53bf07
Alexander Panteleev <unddstr@gmail.com>
2023-03-11 14:23:48 +0300

    Вывести 1-е и 3-е число трехзначного числа

---
+++ Seminar/Seminar2/Example002/Program.cs
@@ -0,0 +1,9 @@
+﻿// 
+Console.Clear();
+int abc = new Random().Next(100, 1000);
+Console.WriteLine($"Случаной сгенированное число {abc}");
+int a = abc / 100;
+int c = abc % 10;
+int result = a*10 +c;
+Console.WriteLine(result);
+
