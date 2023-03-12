d7ad58fafbe8b7a67a6e4cf7dcf1612ff584a961
Alexander Panteleev <unddstr@gmail.com>
2023-03-12 21:43:39 +0300

    Поиск элемента в массиве

---
+++ Lection/Lection2/Example001/Program.cs
@@ -0,0 +1,17 @@
+﻿// 
+Console.Clear();
+int[] array = {11, 21, 31, 41, 15, 61, 4, 17, 18, 19};
+
+int n = array.Length; // n = длинна строки элементов массива
+
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++; //index = index + 1;
+}
