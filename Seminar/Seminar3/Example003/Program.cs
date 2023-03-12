117cfc2f1ba68bf24289a66116d7ab25dce7b4a5
Alexander Panteleev <unddstr@gmail.com>
2023-03-12 15:37:25 +0300

    Ряд квадратов чисел от 1 до n

---
+++ Seminar/Seminar3/Example003/Program.cs
@@ -0,0 +1,16 @@
+﻿//Написать программу, которая на входе принимает число, а на выходе
+// выдает все числа от 1 до n, возведенные в квадрат
+Console.Clear();
+Console.Write("Введите число: ");
+int n;
+int i;
+
+n = Convert.ToInt32(Console.ReadLine());
+i = 1;
+
+
+while (i <= n)
+{
+        Console.Write($"{i*i} ");
+        i++;
+}
