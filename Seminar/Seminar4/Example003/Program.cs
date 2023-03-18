6a887e2e5690c6b2880149a3500713aa0e319381
Alexander Panteleev <unddstr@gmail.com>
2023-03-18 15:31:02 +0300

    Задаем массив случайных дробных чисел

---
+++ Seminar/Seminar4/Example003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+double[] array = new double[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = Math.Round(new Random().NextDouble() * (30 - 10) + 10, 2);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+// Random().NextDouble() - [0, 1]
+// Random().NextDouble() * (end - begin) + begin;
+// end - конечное значение промежутка
+// begin - начальное значение промежутка
