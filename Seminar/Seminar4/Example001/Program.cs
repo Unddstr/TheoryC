2a96bca222f077d0c83a68a388cbf19c55658e90
Alexander Panteleev <unddstr@gmail.com>
2023-03-18 14:42:27 +0300

    Сколько цифр в числе

---
+++ Seminar/Seminar4/Example001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Сколько цифр в числе
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+do{
+    count++;
+    n /= 10;
+}
+while (n > 0);
+Console.WriteLine(count);
