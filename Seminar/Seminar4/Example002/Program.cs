d400125a6d8e0b2e8d8b74a79a42bcdaf947c7e2
Alexander Panteleev <unddstr@gmail.com>
2023-03-18 15:21:42 +0300

    Найти факториал чисел

---
+++ Seminar/Seminar4/Example002/Program.cs
@@ -0,0 +1,12 @@
+﻿// 
+Console.Clear();
+int n = 0;
+int result = 1;
+if (n != 0 && n != 1)
+{
+    for (int i = 2; i <= n; i++)
+    {
+        result = result * i;
+    }
+}
+Console.WriteLine(result);
