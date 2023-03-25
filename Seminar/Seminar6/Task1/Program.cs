41a8f4e56563c05988ce7b77f4e02c5d5720923d
Alexander Panteleev <unddstr@gmail.com>
2023-03-25 14:46:34 +0300

    Задача про треугольник

---
+++ Seminar/Seminar6/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3-е число: ");
+int c = int.Parse(Console.ReadLine()!);
+if (a + b > c && b + c > a && a + c > b)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
