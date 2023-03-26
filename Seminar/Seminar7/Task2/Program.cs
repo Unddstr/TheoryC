69a76a1df27d809d95fd52a9563a16ae58fa34e2
Alexander Panteleev <unddstr@gmail.com>
2023-03-26 14:59:07 +0300

    Возведение элемента двумерного массива в квадрат

---
+++ Seminar/Seminar7/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = i + j;
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 1; i < matrix.GetLength(0); i+=2)
+    {
+        for (int j = 1; j < matrix.GetLength(1); j+=2)
+            matrix[i, j] *= matrix[i, j];
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив");
+PrintMatrix(matrix);
+ReleaseMatrix(matrix);
+Console.WriteLine("Конечный массив");
+PrintMatrix(matrix);
