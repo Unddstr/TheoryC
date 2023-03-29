590e5c964da11d0b8c1280a5f9450cbc1aed29d3
Alexander Panteleev <unddstr@gmail.com>
2023-03-29 22:10:28 +0300

    перевод числа из десятичного в двоичное

---
+++ Seminar/Seminar6/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Перевод числа из десятичного значения в двоичное
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int result = 0, count = 1;
+while (n > 0)
+{
+    result += (n % 2) * count;
+    n /= 2;
+    count *= 10;
+}
+Console.WriteLine(result);
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// string result = string.Empty;
+// while (n > 0)
+// {
+//     result = Convert.ToString(n % 2) + result;
+//     n /= 2;
+// }
+// Console.WriteLine(result);
