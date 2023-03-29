7dfb8e75c78e6d49f80188bd447762c974f916d6
Alexander Panteleev <unddstr@gmail.com>
2023-03-29 23:06:58 +0300

    ряд Фибоначчи

---
+++ Seminar/Seminar6/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывод чисел Фибоначи
+
+int a = 0;
+int b = 1;
+int n = int.Parse(Console.ReadLine()!);
+int c = 0;
+int count = 0;
+Console.Write($"{a} {b} ");
+while (count < n-2)
+{
+   c = a + b;
+   a = b;
+   b = c;
+   count++;
+   Console.Write($"{c} ");
+}
+
