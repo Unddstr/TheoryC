278d573d94116dbd45fe0bd01dcc5b1c4761ac14
Alexander Panteleev <unddstr@gmail.com>
2023-04-01 14:06:16 +0300

    Заполняем двумерный массив

--- Seminar/Seminar7/Task1/Program.cs
+++ Seminar/Seminar7/Task1/Program.cs
@@ -1,4 +1,33 @@
-﻿// 
+﻿// Задать массив размером m*n заполненный случайными числами 
+
+// void InputMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//             matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+//     }
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//             Console.Write($"{matrix[i, j]} \t"); // \t - табуляция что бы каждый
+//                                                 // элемент был друг под другом
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Clear();
+// Console.Write("Введите размеры массива: ");
+// int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+// int[,] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+// PrintMatrix(matrix);
+
+
 void InputMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
