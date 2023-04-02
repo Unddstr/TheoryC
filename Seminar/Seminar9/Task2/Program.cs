b1c7c4c656f25817331da5e8794c1cef813b32f5
Alexander Panteleev <unddstr@gmail.com>
2023-04-02 14:42:57 +0300

    Задача 1

---
+++ Seminar/Seminar9/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿string f(int m, int n)
+{
+    if (m == n)
+        return $"{m} ";
+    return f(m, n - 1) + $"{n} ";
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(m, n));
